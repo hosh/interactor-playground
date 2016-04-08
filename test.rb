e3908b58bd25833fe1cf2902d8a18327f42ab82a
Ho-Sheng Hsiao <talktohosh@gmail.com>
2016-04-08 14:52:41 -0700

    Try using a Simple interactor instead

--- test.rb
+++ test.rb
@@ -12,21 +12,6 @@ class ChargeIntakeFee
   end
 end
 
-# Generic Mail Interactor
-class SendMail
-  attr_reader :mailer_klass, :mailer_method
-
-  def initialize(klass, method_name)
-    @mailer_klass = klass
-    @mailer_method = method_name
-  end
-
-  def call(context)
-    mailer_klass.send(mailer_method, *context)
-    [:ok, context]
-  end
-end
-
 class IntakeMailer
   def self.receipt(inquiry:)
     puts "[intake_mailer] queuing receipt for #{inquiry.id}"
@@ -37,10 +22,11 @@ inquiry = Map.new(id: '123')
 
 # Let's try composition
 sequence = ChargeIntakeFee \
-           | Interactors.new do |context|
-               IntakeMailer.receipt(inquiry: context[:inquiry])
-               [:ok, context]
-             end
+           | Interactors::Simple.new { |context| IntakeMailer.receipt(inquiry: context[:inquiry]) }
+
+# With Simple, as long as no exception is raised, then return [:ok, context]
+# But if anything goes wrong, such as argument error, or whatver, then return the exception
+# This is really meant for something to override the #handle_error method.
 
 puts sequence.inspect
 puts sequence.interactions.inspect
