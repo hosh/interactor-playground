0168e43efc6b08ac3b74f14eb4f3b60253051d0b
Ho-Sheng Hsiao <hosheng.hsiao@legal.io>
2016-04-28 10:57:26 -0700

    Benchmark against mock controller code

--- perf-test.rb
+++ perf-test.rb
@@ -8,8 +8,6 @@ require 'benchmark'
 
 N = 10000
 
-# Simple, naive test
-
 class FunctionalNoOp
   include FunctionalInteractor
 
@@ -18,43 +16,18 @@ class FunctionalNoOp
   end
 end
 
-# Equivalent code:
-# functional_interaction = FunctionalNoOp | FunctionalNoOp | FunctionalNoOp ...
-def functional_interactions
-  (1..9).to_a.inject(FunctionalNoOp) { |interaction, _| interaction | FunctionalNoOp }
-end
-
-class NoOpInteractor
-  include Interactor
-
-  def call
-  end
-end
-
-class NoOpOrganizer
-  include Interactor::Organizer
-  organize NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor
-end
-
-puts "========"
-puts "NoOp interactors chained 10, N=#{N}"
-# Tries to minimize garbage collection effects
-# http://ruby-doc.org/stdlib-2.3.0/libdoc/benchmark/rdoc/Benchmark.html#method-c-bmbm
-Benchmark.bmbm(15) do |x|
-  x.report('collective_idea') { N.times { NoOpOrganizer.call } }
-  x.report('functional')      { N.times { functional_interactions.call } }
-end
-
-
-puts "========"
-puts "NoOp + FailureOp (5th) interactors chained 10, N=#{N}"
-
 class FunctionalFailureOp
   include FunctionalInteractor
 
   def call(context={})
     [:error, :error]
   end
+end
+
+# Equivalent code:
+# functional_interaction = FunctionalNoOp | FunctionalNoOp | FunctionalNoOp ...
+def functional_interactions
+  (1..9).to_a.inject(FunctionalNoOp) { |interaction, _| interaction | FunctionalNoOp }
 end
 
 def functional_failure
@@ -71,7 +44,14 @@ def functional_failure
 end
 
 class FunctionalController
-  def self.action
+  def self.successful_action
+    Kase.kase functional_interactions.call do
+      on(:ok)    { |context| context }
+      on(:error) { |reason| report_error(reason) }
+    end
+  end
+
+  def self.failing_action
     Kase.kase functional_failure.call do
       on(:ok)    { |context| context }
       on(:error) { |reason| report_error(reason) }
@@ -83,6 +63,13 @@ class FunctionalController
   end
 end
 
+class NoOpInteractor
+  include Interactor
+
+  def call
+  end
+end
+
 class FailureInteractor
   include Interactor
 
@@ -91,13 +78,23 @@ class FailureInteractor
   end
 end
 
+class NoOpOrganizer
+  include Interactor::Organizer
+  organize NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor
+end
+
 class FailureOrganizer
   include Interactor::Organizer
   organize NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, FailureInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor, NoOpInteractor
 end
 
 class OOPController
-  def self.action
+  def self.successful_action
+    result = NoOpOrganizer.call
+    report_error(result.error) unless result.success?
+  end
+
+  def self.failing_action
     result = FailureOrganizer.call
     report_error(result.error) unless result.success?
   end
@@ -107,7 +104,20 @@ class OOPController
   end
 end
 
+
+puts "========"
+puts "NoOp interactors chained 10, N=#{N}"
+# Tries to minimize garbage collection effects
+# http://ruby-doc.org/stdlib-2.3.0/libdoc/benchmark/rdoc/Benchmark.html#method-c-bmbm
 Benchmark.bmbm(15) do |x|
-  x.report('collective_idea') { N.times { OOPController.action } }
-  x.report('functional')      { N.times { FunctionalController.action } }
+  x.report('collective_idea') { N.times { OOPController.successful_action } }
+  x.report('functional')      { N.times { FunctionalController.successful_action } }
+end
+
+
+puts "========"
+puts "NoOp + FailureOp (5th) interactors chained 10, N=#{N}"
+Benchmark.bmbm(15) do |x|
+  x.report('collective_idea') { N.times { OOPController.failing_action } }
+  x.report('functional')      { N.times { FunctionalController.failing_action } }
 end
